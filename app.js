f4c5d281041326c0a91dff5e91b2ad2e2d33d328
Grégory <claudejurado@orange.fr>
2017-03-31 15:22:37 +0200

    intergration du plug in datatables, et modification du tableau pour pouvoir trier par nom, prenom et age, reintegrer le boutton effacer

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
 
+
+
+
+
 var annuaire = [];
-
-
-
-
+$(document).ready(function (){
 	$("#enter").click(function(){
-		var name = $("#inputName").val();
-		var firstname = $("#inputFirstName").val();
-		var age = $("#inputAge").val();
-		var identity = {"name": name, "firstname":firstname, "age": age};
-		annuaire.push(identity);
-		$("tbody").append($("<tr><td>"+name+"</td><td>"+firstname+"</td><td>"+age+"</td><td><button id='delete'>supprimer</button></td>"));		
-		localStorage.setItem(JSON.stringify(identity));	
+	var name = $("#inputName").val();
+	var firstname = $("#inputFirstName").val();
+	var age = $("#inputAge").val();
+	var identity = {"name": name, "firstname":firstname, "age": age};
+	annuaire.push(identity);
+	$("input").val("");
+	//$("tbody").append($("<tr><td>"+name+"</td><td>"+firstname+"</td><td>"+age+"</td><td><button id='delete'>supprimer</button></td></tr>"));		
+	$("#dataTable").DataTable().row.add([
+		name,
+		firstname,
+		age
+		]).draw(false);
+	})
+	$("#enter").click();
 });
-
 	
 $("tbody").delegate("#delete","click",function(){
 
 	$(this).parent().parent().remove();
 
-})
+});
+
+$(document).ready(function(){
+		$("#dataTable").DataTable();
+		$("#dataTable").DataTable({
+			"scrollX": true
+	})
+});
+
+
 
 
 	//$("tbody").delegate("#delete","click",function(){
