48777cb546ef57c4519dd0b5d8ec44ce0ecd82bb
Grégory <claudejurado@orange.fr>
2017-03-31 17:27:56 +0200

    mise en place de la sauvegarde dans le localstorage mise en place du retour du local storage mettre en place l'affichage de la sauvegarde de local storage

--- app.js
+++ app.js
@@ -1,54 +1,62 @@
-
-
-
-
-
 var annuaire = [];
+
+function save(){
+
+	localStorage.setItem("annuaire",JSON.stringify(annuaire));
+
+}
+
+function load(){
+	var a = localStorage.getItem("annuaire");
+
+	if (a != null){
+		annuaire = JSON.parse(a);
+	}
+	console.log(annuaire)
+}
+
+
+
 $(document).ready(function (){
+
+	load();
+	
 	$("#enter").click(function(){
-	var name = $("#inputName").val();
-	var firstname = $("#inputFirstName").val();
-	var age = $("#inputAge").val();
-	var buttDel = "<button id='delete'>del</button>";
-	var identity = {"name": name, "firstname":firstname, "age": age};
-	annuaire.push(identity);
-	console.log(annuaire);
-	$("input").val("");		
-	$("#dataTable").DataTable().row.add([
-		name,
-		firstname,
-		age,
-		buttDel
-		]).draw(false);
+	
+		var name = $("#inputName").val();
+		var firstname = $("#inputFirstName").val();
+		var age = $("#inputAge").val();
+		var buttDel = "<button id='delete'>del</button>";
+		var identity = {"name": name, "firstname":firstname, "age": age};
+		annuaire.push(identity);
+		save();
+		load();
+		console.log(annuaire);
+		$("input").val("");		
+		$("#dataTable").DataTable().row.add([
+			name,
+			firstname,
+			age,
+			buttDel
+			]).draw(false);
 	})
+
 });
+
 $(document).ready(function(){
+
 	$("#dataTable").on("click","button",function(){
+
 		$("#dataTable").DataTable()
 			.row($(this).parent().parent("tr"))
 			.remove()
 			.draw();		
 		annuaire.splice($(this).parent().parent().data(),1);
+
 		console.log(annuaire)
-
-
 	})
 });	
 
-
-// $("tbody").delegate("#delete","click",function(){
-
-// 	$("#dataTable").DataTable().row.remove([
-// 		name,
-// 		firstname,
-// 		age,
-// 		buttDel]).draw(false);
-
-	//$(this).parent().parent().remove();
-//	annuaire.remove(identity);
-
-
-//});
 
 $(document).ready(function(){
 		$("#dataTable").DataTable();
@@ -56,11 +64,3 @@ $(document).ready(function(){
 			"scrollX": true
 	})
 });
-
-
-
-
-	//$("tbody").delegate("#delete","click",function(){
-	//	$("tr").remove();
-	//}); 
-	//permet de reinitialiser le tableau 
